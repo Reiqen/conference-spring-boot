b93fa626ca8d1f4ae9b0bd17a36b505332e49513
Roman Kuznetcov <rkuznetsov@neoflex.ru>
2021-04-29 13:41:42 +0300

    Comment db settings

--- src/main/java/ru/neoflex/conferencespringboot/config/PersistenceConfiguration.java
+++ src/main/java/ru/neoflex/conferencespringboot/config/PersistenceConfiguration.java
@@ -9,12 +9,12 @@ import javax.sql.DataSource;
 @Configuration // Аннотация показывает, что бины из этого класса используются для конфигурации проекта
 public class PersistenceConfiguration {
 
-    @Bean // Этот бин будет использован для конфигурации ресурса данных БД (добавление URL, логина и пароля)
-    public DataSource dataSource() {
+    /* @Bean // Этот бин будет использован для конфигурации ресурса данных БД (добавление URL, логина и пароля)
+     public DataSource dataSource() {
         DataSourceBuilder builder = DataSourceBuilder.create();
         builder.url("jdbc:postgresql://localhost:5432/conference");
         builder.username("postgres");
         builder.password("postgres");
         return builder.build();
-    }
+    }- */
 }
